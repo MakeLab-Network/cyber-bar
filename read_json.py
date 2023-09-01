40b81fc49d4e5e112517fd2cde9d1f80e0486e16
yuval-dimri <yuval5363@gmail.com>
2023-09-01 21:49:37 +0300

    moved to db folder

--- read_json.py
+++
@@ -1,9 +0,0 @@
-import json
-with open("texts\\recipes.json", 'rb') as f:
-   recipes = json.load(f)
-recepies = recipes['10_dispensers']
-cocktails = recipes['cocktails']
-ingredients = recipes['ingredients']
-with open("texts\\questions1.json", 'rb') as f:
-   questions = json.load(f)
-questions = questions['questions']
