b493ffd3969053b4962edbd1e085b88aaa5c890a
yuval-dimri <yuval5363@gmail.com>
2023-09-01 23:39:23 +0300

    app is reading text and stuff

--- web_app/app.py
+++ web_app/app.py
@@ -1,52 +1,38 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
+from db_access import Questions as DBQuestions
+from speak import speak
+
 
 app = Flask(__name__, template_folder='quiz_app')
+app.secret_key = 'asdflkj'  # Change this to a secure secret key
 
-questions = [
-    {
-        'question_number': 1,
-        'question_text': 'Who was the first King of the Vikings?',
-        'correct_answer': 'Harald Fairhair',
-        'options': ['Ragnar Lothbrok', 'Leif Erikson', 'Harald Fairhair', 'Ivar the Boneless']
-    },
-    {
-        'question_number': 2,
-        'question_text': 'Who was the first King of the Vikings?',
-        'correct_answer': 'Harald Fairhair',
-        'options': ['1', '2', '3', '4']
-    },
-    {
-        'question_number': 3,
-        'question_text': 'Who was the first King of the Vikings?',
-        'correct_answer': 'Harald Fairhair',
-        'options': ['Ragnar Lothbrok', 'Leif Erikson', 'Harald Fairhair', 'Ivar the Boneless']
-    },
-    # Add more questions here
-]
+question_generator = DBQuestions.get_random_questions(20)
+# def get_question_generator():
+#     if 'question_generator_index' not in session:
+#         session['question_generator_index'] = 0
+#     if 'question_generator' not in session:
+#         session['question_generator'] = list(
+#             DBQuestions.get_random_questions(20))
+#     return session['question_generator']
 
-current_question_index = 0
 
 @app.route('/')
 def index():
-    current_question = questions[current_question_index]
-    return render_template('quiz.html', question=current_question)
+    # question_generator = get_question_generator()
+    # current_index = session['question_generator_index']
+    # current_question = question_generator?
+    return render_template('quiz.html', question=next(question_generator))
+
 
 @app.route('/handle_button_press/', methods=['POST'])
 def handle_button_press():
-    global current_question_index  # Use the global variable
-    
-    selected_option = request.form.get('selected_option')
-    print(selected_option)
-    # current_question = questions[current_question_index]
-    
-    # if selected_option == current_question['correct_answer']:
-        # current_question_index += 1
-    
-    # if current_question_index < len(questions):
-        # next_question = questions[current_question_index]
-        # return render_template('quiz.html', question=next_question)
-    # else:
-        # return render_template('quiz_complete.html')
+    try:
+        next_question = next(question_generator)
+        speak(next_question["question"])
+        return render_template('quiz.html', question=next_question)
+    except StopIteration:
+        return render_template('quiz_end.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
