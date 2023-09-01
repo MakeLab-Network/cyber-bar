84abc7851dbdb818e6c028fa5478fd262e8e62d6
yuval-dimri <yuval5363@gmail.com>
2023-09-01 23:54:19 +0300

    root now redirects to the questions

--- web_app/app.py
+++ web_app/app.py
@@ -1,27 +1,18 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from db_access import Questions as DBQuestions
 from speak import speak
 
 
 app = Flask(__name__, template_folder='quiz_app')
-app.secret_key = 'asdflkj'  # Change this to a secure secret key
 
 question_generator = DBQuestions.get_random_questions(20)
-# def get_question_generator():
-#     if 'question_generator_index' not in session:
-#         session['question_generator_index'] = 0
-#     if 'question_generator' not in session:
-#         session['question_generator'] = list(
-#             DBQuestions.get_random_questions(20))
-#     return session['question_generator']
 
 
 @app.route('/')
 def index():
-    # question_generator = get_question_generator()
-    # current_index = session['question_generator_index']
-    # current_question = question_generator?
-    return render_template('quiz.html', question=next(question_generator))
+    next_question = next(question_generator)
+    speak(next_question["question"])
+    return render_template('quiz.html', question=next_question)
 
 
 @app.route('/handle_button_press/', methods=['POST'])
@@ -35,4 +26,4 @@ def handle_button_press():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
