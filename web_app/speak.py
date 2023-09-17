8ec899a686ca5a67f8e999a97e83773d31763396
yuval-dimri <yuval5363@gmail.com>
2023-09-17 12:20:01 +0300

    now using gtts

--- web_app/speak.py
+++ web_app/speak.py
@@ -1,12 +1,12 @@
-import pyttsx3
-import threading
+from gtts import gTTS
+import os
+import playsound
 
 
 def speak(text):
-    def speak_thread(text):
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-        # Create a new thread to run the text-to-speech engine
-    t = threading.Thread(target=speak_thread, args=(text,))
-    t.start()
+    tts = gTTS(text=text, lang='fr')
+
+    filename = "tmp-text.mp3"
+    tts.save(filename)
+    playsound.playsound(filename, block=False)
+    os.remove(filename)
