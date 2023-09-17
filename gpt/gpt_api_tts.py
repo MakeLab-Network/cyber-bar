e1f9acdd2f0b6388a19732e3e3788ea731ca69dd
guyostfeld <guyostfeld1@gmail.com>
2023-09-17 22:54:40 +0300

    WIP

--- gpt/gpt_api_tts.py
+++ gpt/gpt_api_tts.py
@@ -7,10 +7,14 @@ import os
 import time
 from gtts import gTTS
 from pygame import mixer
+from pydub import AudioSegment
+from pydub.playback import play
+
 mixer.init()
 openai.api_key = os.environ.get("openai_key")
+output_path = r"C:\temp\output.mp3"
 
-def play_tts(text, end_line=True):
+def create_tts_file(text, output_path, end_line=True):
 	try:
 		text = text.rstrip().lstrip()
 		while(mixer.music.get_busy()):
@@ -18,16 +22,35 @@ def play_tts(text, end_line=True):
 			pass
 		mixer.music.unload()
 		try:
-			os.remove("tts.mp3")
+			os.remove(output_path)
 		except:
 			pass
 		tts = gTTS(text)
-		tts.save("tts.mp3")
-		mixer.music.load("tts.mp3")
+		tts.save(output_path)
 		print(text, end=('\n' if end_line else ' '))
+	except Exception as e:
+		print(e)
+
+def play_file(path):
+	try:
+		mixer.music.load(path)
+		# print(text, end=('\n' if end_line else ' '))
 		mixer.music.play()
-	except:
-		pass
+	except Exception as e:
+		print(e)
+
+def minionify_voice(input_file, output_file):
+    sound = AudioSegment.from_file(input_file, format="mp3")
+    
+    # Adjust speed (increase speed for Minion effect)
+    sound = sound.speedup(playback_speed=1.5)
+
+    # Adjust pitch (increase pitch for Minion effect)
+    sound = sound._spawn(sound.raw_data, overrides={
+       "frame_rate": int(sound.frame_rate * 1.5)
+    }).set_frame_rate(sound.frame_rate)
+    
+    sound.export(output_file, format="mp3")
 
 all_chunks=""
 def ask_gpt(prompt, model="gpt-3.5-turbo"):
@@ -42,10 +65,12 @@ def ask_gpt(prompt, model="gpt-3.5-turbo"):
 		chunks += response_text
 		all_chunks += response_text
 		if '\n' in chunks or '.' in chunks or ',' in chunks or i > 100:
-			play_tts(chunks, end_line=('\n' in chunks))
+			create_tts_file(chunks, output_path, end_line=('\n' in chunks))
+			play_tts(output_path)
 			chunks = ""
 			i = 0
-	play_tts(chunks)
+	create_tts_file(chunks, output_path, end_line=('\n' in chunks))
+	play_tts(output_path)
 	# return all_chunks
 	# return response_text
 
